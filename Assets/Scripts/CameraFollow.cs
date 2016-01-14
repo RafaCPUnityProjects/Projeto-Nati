cf4c575f2783d350a7d8642d92bd440eb67aca21
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-14 10:45:12 -0300

    Player movement mechanics started, 2 camera scene created

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 relativeStartPos;
+    // Use this for initialization
+    void Start()
+    {
+        relativeStartPos = target.position - transform.position;
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position - relativeStartPos;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public string playerNumber = "1";
+    public float moveSpeed = 0.1f;
+    public float rotateSpeed = 2.0f;
+    public float jumpForce = 10.0f;
+    public float isGroundedSize = 1f;
+
+    Rigidbody myRigidBody;
+    string xAxisName;
+    string yAxisName;
+    string jumpButton;
+    string grabButton;
+    string hitButton;
+    bool jumping = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        xAxisName = "L_XAxis_" + playerNumber;
+        yAxisName = "L_YAxis_" + playerNumber;
+        jumpButton = "A_" + playerNumber;
+        grabButton = "X_" + playerNumber;
+        hitButton = "B_" + playerNumber;
+
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string xAxisName = "L_XAxis_" + playerNumber;
+        string yAxisName = "L_YAxis_" + playerNumber;
+        string jumpButton = "A_" + playerNumber;
+        string grabButton = "X_" + playerNumber;
+        string hitButton = "B_" + playerNumber;
+
+        float xAxisValue = Input.GetAxis(xAxisName);
+        float yAxisValue = Input.GetAxis(yAxisName);
+
+        transform.Rotate(Vector3.up, xAxisValue * rotateSpeed);
+
+        float jump = 0.0f;
+
+        if (Input.GetButtonDown(jumpButton) && IsGrounded())
+        {
+            jumping = true;
+            jump = jumpForce;
+        }
+
+        float speed = yAxisValue * moveSpeed;
+        Vector3 movementVector = new Vector3(speed, jump, 0);
+
+        Vector3 forwardVector = transform.TransformDirection(Vector3.forward);
+
+
+        myRigidBody.AddRelativeForce(movementVector);
+    }
+
+    bool IsGrounded()
+    {
+        Vector3 down = transform.TransformDirection(Vector3.down);
+        return Physics.Raycast(transform.position, down, isGroundedSize);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector3 direction = transform.TransformDirection(Vector3.down) * isGroundedSize;
+        Gizmos.DrawRay(transform.position, direction);
+    }
+}
