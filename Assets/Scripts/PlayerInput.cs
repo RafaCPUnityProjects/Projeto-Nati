b22bba2952e39b7924eeee7775645efed8bc22a1
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-14 21:20:53 -0300

    Tutorial online https://www.youtube.com/watch?v=OBtaLCmJexk&index=2&list=PLFt_AvWsXl0f0hqURlhyIoAabKPgRsqjz

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour
+{
+    PlayerController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
