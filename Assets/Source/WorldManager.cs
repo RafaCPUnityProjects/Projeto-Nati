ba200503d7e5390a6ac9bcc87f333dcc1aa182c1
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-18 16:00:51 -0300

    Tutorial de voxel https://www.youtube.com/watch?v=C8rzFWMJApk&list=PLlTlfj-RDVxlwVDBljKPDvYSinjXsjlTn

---
+++ Assets/Source/WorldManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour
+{
+
+    private static WorldManager _Instance;
+
+    void Start()
+    {
+        _Instance = this;
+    }
+
+    void Update()
+    {
+
+    }
+}
