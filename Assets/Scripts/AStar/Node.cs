bc5deb6fd95f629d2757ddb495d44037fffa09fb
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-21 11:49:55 -0300

    A Star Tutorial - difude

---
+++ Assets/Scripts/AStar/Node.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Node : IHeapItem<Node>
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+
+    public Node parent;
+
+    int heapIndex;
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+    public int CompareTo(Node other)
+    {
+        int compare = fCost.CompareTo(other.fCost);
+        if(compare == 0)
+        {
+            compare = hCost.CompareTo(other.hCost);
+        }
+        return -compare;
+    }
+}
---
+++ Assets/Scripts/AStar/Pathfinding.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+[RequireComponent(typeof(Grid))]
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    Grid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FindPath(seeker.position, target.position);
+        }
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node currentNode = openSet.RemoveFirst();
+            //for (int i = 1; i < openSet.Count; i++)
+            //{
+            //    if(openSet[i].fCost < currentNode.fCost || (openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost))
+            //    {
+            //        currentNode = openSet[i];
+            //    }
+            //}
+            //openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if(currentNode == targetNode)
+            {
+                sw.Stop();
+                print("Path found in: " + sw.ElapsedMilliseconds + " ms");
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (Node neighbourNode in grid.GetNeighbours(currentNode))
+            {
+                if (!neighbourNode.walkable || closedSet.Contains(neighbourNode))
+                {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbourNode);
+                if(newMovementCostToNeighbour < neighbourNode.gCost || !openSet.Contains(neighbourNode))
+                {
+                    neighbourNode.gCost = newMovementCostToNeighbour;
+                    neighbourNode.hCost = GetDistance(neighbourNode, targetNode);
+
+                    neighbourNode.parent = currentNode;
+                    if(!openSet.Contains(neighbourNode))
+                    {
+                        openSet.Add(neighbourNode);
+                    }
+                }
+            }
+        }
+    }
+
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+        while(currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if(dstX > dstY)
+        {
+            return 14 * dstY + 10 * (dstX - dstY);
+        }
+        else
+        {
+            return 14 * dstX + 10 * (dstY - dstX);
+        }
+    }
+}
