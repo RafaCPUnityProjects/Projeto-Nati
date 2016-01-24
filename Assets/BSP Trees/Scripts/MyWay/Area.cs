488a25651710b859d26ef26b46f2836e4aabd509
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-24 15:50:58 -0300

    Tentativa de fazer o BSP

--- Assets/BSP Trees/Scripts/MyWay/Area.cs
+++ Assets/BSP Trees/Scripts/MyWay/Area.cs
@@ -11,6 +11,7 @@ public class Area
     public int sizeY;
 
     public Coord center;
+    public Coord size;
 
     public Area()
     {
@@ -26,12 +27,59 @@ public class Area
         this.sizeY = sizeY;
 
         center = new Coord(startX + sizeX / 2, startY + sizeY / 2);
+        size = new Coord(sizeX, sizeY);
     }
 
-    public List<Area> Split()
+    public List<Area> Split(int minXSize, int minYSize)
     {
         List<Area> newAreas = new List<Area>();
 
+        bool splitH = BSPManager.random.NextDouble() > 0.5f;
+        if (sizeX / sizeY > 1.25f)
+        {
+            splitH = true;
+        }
+        else if (sizeY / sizeX > 1.25f)
+        {
+            splitH = false;
+        }
+        int max;
+        int splitPos;
+        if (splitH)
+        {
+            max = sizeY - minYSize;
+            if (max <= minYSize)
+            {
+                return newAreas;
+            }
+            else
+            {
+                splitPos = BSPManager.random.Next(minYSize, max);
+            }
+        }
+        else
+        {
+            max = sizeX - minXSize;
+            if (max <= minXSize)
+            {
+                return newAreas;
+            }
+            else
+            {
+                splitPos = BSPManager.random.Next(minXSize, max);
+            }
+        }
+
+        if (splitH)
+        {
+            newAreas.Add(new Area(startX, startY, sizeX, splitPos));
+            newAreas.Add(new Area(startX, startY + splitPos, sizeX, sizeY - splitPos));
+        }
+        else
+        {
+            newAreas.Add(new Area(startX, startY, splitPos, sizeY));
+            newAreas.Add(new Area(startX + splitPos, startY, sizeX - splitPos, sizeY));
+        }
         return newAreas;
     }
 
@@ -51,9 +99,9 @@ public class Area
             this.y = y;
         }
 
-        public Vector3 Coord2Vector3(Coord coord)
+        public Vector3 Coord2Vector3()
         {
-            return new Vector3(coord.x, 0, coord.y);
+            return new Vector3(x, 1, y);
         }
     }
 
---
+++ Assets/BSP Trees/Scripts/MyWay/BSPManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class BSPManager : MonoBehaviour
+{
+    public static string seed;
+    public static System.Random random = new System.Random(seed.GetHashCode());
+    public int width = 80;
+    public int height = 100;
+    public int maxXSize = 20;
+    public int maxYSize = 25;
+    public int minXSize = 8;
+    public int minYSize = 10;
+
+    List<Area> areas = new List<Area>();
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    private void GenerateMap()
+    {
+        Area startingArea = new Area(width / 2, height / 2, width, height);
+        areas.Clear();
+        areas.Add(startingArea);
+        while (true)
+        {
+            for (int i = 0; i < areas.Count; i++)
+            {
+                List<Area> newAreas = areas[i].Split(minXSize, minYSize);
+                if (newAreas == null || newAreas.Count < 1)
+                {
+                    break;
+                }
+                else
+                {
+                    areas.AddRange(areas[i].Split(minXSize, minYSize));
+                    areas.Remove(areas[i]);
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(areas != null && areas.Count > 1)
+        {
+            for (int i = 0; i < areas.Count; i++)
+            {
+                print("DrawGizmos: " + i);
+                Gizmos.color = Color.white;
+                Gizmos.DrawCube(areas[i].center.Coord2Vector3(),areas[i].size.Coord2Vector3());
+            }
+        }
+    }
+}
