8d992c42dd19f23027cfb2a0ee7222ff4c120688
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-01-26 14:04:58 -0300

    Tiled2Unity test

---
+++ Assets/TiledTest/Scripts/GenerateRandomMap.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GenerateRandomMap : MonoBehaviour
+{
+    public string seed;
+    public bool useRandomSeed = false;
+
+    public GameObject[] room3x3;
+    public GameObject[] room5x5;
+    public GameObject[] room7x7;
+    public GameObject[] room10x10;
+
+    System.Random random;
+
+    void Start()
+    {
+        InitializeSeed();
+        GenerateMap();
+    }
+
+    void InitializeSeed()
+    {
+        if(useRandomSeed)
+        {
+            seed += System.DateTime.Now.ToString();
+            print("Random seed = " + seed);
+        }
+        random = new System.Random(seed.GetHashCode());
+    }
+
+    private void GenerateMap()
+    {
+        
+    }
+
+}
---
+++ Assets/TiledTest/Scripts/Room2D.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room2D
+{
+    public int sizeX, sizeY;
+    public GameObject prefab;
+}
