c943fc6b91b647882ad27d87d2884dcbbdc4b45b
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-02-14 14:41:31 -0300

    Perlin Noise

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using PerlinNoise;
+
+[CustomEditor(typeof(PerlinNoise.MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        PerlinNoise.MapGenerator mapGen = (PerlinNoise.MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if(mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+
+}
